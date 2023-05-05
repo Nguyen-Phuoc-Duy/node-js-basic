24b8c59cc9f15649ed63cf9a22f289944ac2a2b3
PhuocDuy <nguyenphuocduy2908@gmail.com>
2023-05-05 12:35:00 +0700

    #13

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,4 +1,5 @@
 // import connection from "../configs/connectDB.js";
+
 import pool from "../configs/connectDB.js";
 let getHomePage = async (rew, res) => {
   // simple query
@@ -55,5 +56,55 @@ let createNewUser = async (req, res) => {
   );
   return res.redirect("/");
 };
+let deleteUser = async (req, res) => {
+  let userId = req.body.userId;
+  await pool.execute("delete from users where id = ?", [userId]);
+  // return res.send(`deleteUser ${req.body.userId}`);
+  return res.redirect("/");
+};
+let deleteUser2 = async (req, res) => {
+  let userId = req.params.id;
+  await pool.execute("delete from users where id = ?", [userId]);
+  // return res.send(`deleteUser ${req.body.userId}`);
+  return res.redirect("/");
+};
+let getEditPage = async (req, res) => {
+  let id = req.params.id;
+  let [user] = await pool.execute(`SELECT * FROM users where id = ?`, [id]);
+  // return res.send(`update ${req.params.id}`);
+  // return res.send(JSON.stringify(user));
+  return res.render("update.ejs", { dataUser: user[0] });
+};
+let postUpdateUser = async (req, res) => {
+  let { firstName, lastName, email, address, id } = req.body;
+  await pool.execute(
+    `update users set firstName= ?, lastName= ?, email= ?, address= ? where id= ?`,
+    [firstName, lastName, email, address, id]
+  );
+  // console.log("check", req.body);
+  return res.redirect("/");
+  // return res.send(`save`);
+};
+let postUpdateUser2 = async (req, res) => {
+  let { firstName, lastName, email, address } = req.body;
+  let id = req.params.id;
+  // console.log("iddddddddddd", id);
+  await pool.execute(
+    "update users set firstName= ?, lastName= ?, email= ?, address= ? where id= ?",
+    [firstName, lastName, email, address, id]
+  );
+  // console.log("check", req.body);
+  return res.redirect("/");
+  // return res.send(`save`);
+};
 // module.exports = { getHomePage };
-export default { getHomePage, getDetailPage, createNewUser };
+export default {
+  getHomePage,
+  getDetailPage,
+  createNewUser,
+  deleteUser,
+  getEditPage,
+  postUpdateUser,
+  deleteUser2,
+  postUpdateUser2,
+};
--- src/route/web.js
+++ src/route/web.js
@@ -8,6 +8,11 @@ const initWebRoute = (app) => {
   //   });
   router.get("/detail/user/:id", homeController.getDetailPage);
   router.post("/create-new-user", homeController.createNewUser);
+  router.post("/delete-user", homeController.deleteUser);
+  router.post("/delete-user/:id", homeController.deleteUser2);
+  router.get("/edit-user/:id", homeController.getEditPage);
+  router.post("/update-user", homeController.postUpdateUser);
+  router.post("/update-user/:id", homeController.postUpdateUser2);
   router.get("/", homeController.getHomePage);
   router.get("/home", (req, res) => {
     res.send("Hello World! Phuong Phuong");
