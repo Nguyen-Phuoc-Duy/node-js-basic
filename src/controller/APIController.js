f183ffd92e81dd033722b0d63333e35fe1bd42f0
PhuocDuy <nguyenphuocduy2908@gmail.com>
2023-05-16 10:45:22 +0700

    #14

---
+++ src/controller/APIController.js
@@ -0,0 +1,53 @@
+import pool from "../configs/connectDB.js";
+let getAllUsers = async (req, res) => {
+  const [rows, fields] = await pool.execute("SELECT * FROM `users`");
+  return res.status(200).json({
+    ////json api
+    messge: "ok",
+    data: rows,
+  });
+};
+let createNewUser = async (req, res) => {
+  let { firstName, lastName, email, address } = req.body;
+  if (!firstName || !lastName || !email || !address) {
+    return res.status(200).json({
+      messge: "missing required params",
+    });
+  }
+  await pool.execute(
+    "insert into users(firstName, lastName, email, address) values (?, ?, ?, ?)",
+    [firstName, lastName, email, address]
+  );
+  return res.status(200).json({
+    messge: "ok111",
+  });
+};
+let updateUser = async (req, res) => {
+  let { firstName, lastName, email, address, id } = req.body;
+  if (!firstName || !lastName || !email || !address || !id) {
+    return res.status(200).json({
+      messge: "missing required params",
+    });
+  }
+  await pool.execute(
+    `update users set firstName= ?, lastName= ?, email= ?, address= ? where id= ?`,
+    [firstName, lastName, email, address, id]
+  );
+  return res.status(200).json({
+    messge: "ok222",
+  });
+};
+let deleteUser = async (req, res) => {
+  let userId = req.params.id;
+  if (!userId) {
+    return res.status(200).json({
+      messge: "missing required params",
+    });
+  }
+  await pool.execute("delete from users where id = ?", [userId]);
+  return res.status(200).json({
+    messge: "ok333",
+  });
+};
+
+export default { getAllUsers, createNewUser, updateUser, deleteUser };
